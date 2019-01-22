de187f16991f225a6eea12a444b98db4c0f7ba46
Huanjia Liang <huanjial@uci.edu>
2019-01-21 16:58:11 -0800

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Huanjia Liang on 1/21/19.
+//  Copyright © 2019 Huanjia Liang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
